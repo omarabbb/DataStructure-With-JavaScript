819bf99488e544a8a1512f26e954f178880dbd1a
Mohamed Abdelazim <mabdelazemahmed@gmail.com>
2023-12-01 19:23:03 +0200

    addding basic Stack

---
+++ JS/Stack.js
@@ -0,0 +1,27 @@
+const LinkedList = require("./LinkedList");
+
+class Stack {
+  constructor() {
+    this.dataStore = new LinkedList();
+  }
+
+  push(data) {
+    this.dataStore.pushIntoHead(data);
+  }
+
+  pop() {
+    this.dataStore.deleteNode(this.dataStore.headNode.data);
+  }
+
+  peek() {
+    return this.dataStore.headNode;
+  }
+
+  printStack() {
+    return this.dataStore.printList();
+  }
+
+  isEmpty() {
+    return this.dataStore.hasData() ? false : true;
+  }
+}
